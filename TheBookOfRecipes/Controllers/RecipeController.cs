b2c188f02c1837b6448e098929f961ece92ae478
DaniDagami <daniel.g.boychev@gmail.com>
2025-02-09 14:46:14 +0200

    Added Recipe Controller

---
+++ TheBookOfRecipes/Controllers/RecipeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheBookOfRecipes.Data;
+using TheBookOfRecipes.Models; 
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBookOfRecipes.Controllers {
+    public class RecipesController : Controller {
+        private readonly ApplicationDbContext _context;
+
+        public RecipesController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        // GET: Recipes
+        public async Task<IActionResult> Index() {
+            var recipes = await _context.Recipes.Include(r => r.Category).ToListAsync();
+            return View(recipes);
+        }
+
+        // GET: Recipes/Create
+        public IActionResult Create() {
+            ViewBag.Categories = _context.Categories.ToList(); 
+            return View();
+        }
+
+        // POST: Recipes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Recipe recipe) {
+            if(ModelState.IsValid) {
+                _context.Add(recipe);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Categories = _context.Categories.ToList();
+            return View(recipe);
+        }
+
+    }
+}
