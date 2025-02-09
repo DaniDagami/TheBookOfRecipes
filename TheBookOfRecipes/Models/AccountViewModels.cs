b1c419eb1e9a8416cb4f12853d15a4f6074e73d2
DaniDagami <daniel.g.boychev@gmail.com>
2025-02-09 15:31:58 +0200

    accountviewmodels

--- TheBookOfRecipes/Models/AccountViewModels.cs
+++ TheBookOfRecipes/Models/AccountViewModels.cs
@@ -1,4 +1,31 @@
-﻿namespace TheBookOfRecipes.Models {
-    public class AccountViewModel {
-    }
+﻿using System.ComponentModel.DataAnnotations;
+
+public class RegisterViewModel {
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
+
+public class LoginViewModel {
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
 }
