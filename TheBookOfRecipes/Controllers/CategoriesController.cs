9bb243d8b762ebee0a13a53a1a2a5dff1d1e78d9
DaniDagami <daniel.g.boychev@gmail.com>
2025-02-11 17:12:25 +0200

    Added Categories view

---
+++ TheBookOfRecipes/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBookOfRecipes.Data;
+using TheBookOfRecipes.Models;
+
+public class CategoriesController : Controller {
+    private readonly ApplicationDbContext _context;
+
+    public CategoriesController(ApplicationDbContext context) {
+        _context = context;
+    }
+
+    // GET: Categories
+    public async Task<IActionResult> Index() {
+        var categories = await _context.Categories.ToListAsync();
+        return View(categories);
+    }
+
+    // GET: Categories/Recipes/5
+    public async Task<IActionResult> Recipes(int id) {
+        var category = await _context.Categories
+            .Include(c => c.Recipes)
+            .FirstOrDefaultAsync(c => c.CategoryId == id);
+
+        if(category == null) {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+}
